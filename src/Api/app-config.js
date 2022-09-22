9a3b585a0809a28e27f7a085fbdc2775141b1045
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2022-09-22 18:13:02 +0900

    Update app-config.js

--- src/Api/app-config.js
+++ src/Api/app-config.js
@@ -3,7 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  backendHost = "http://222.118.103.229";
+  backendHost = "http://222.118.103.229:80";
 }
 
 export const API_BASE_URL = `${backendHost}`;
