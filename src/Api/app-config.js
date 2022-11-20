8d5753f62fd6fc53cdc14b2513e65cd3a7cbfb10
limjongheok <jjongbbang2@naver.com>
2022-11-21 01:01:34 +0900

    상품 수정 이미지

--- src/Api/app-config.js
+++ src/Api/app-config.js
@@ -2,5 +2,5 @@ let backendHost;
 
 // backendHost = "http://222.118.103.229:80";
 backendHost = "https://hannam.shop";
-
+//backendHost = "http://localhost:8080";
 export const API_BASE_URL = `${backendHost}`;
